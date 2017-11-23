d89fe744b341d30eb1004b1abcbafcf71f66dbef
Maxim <max-navsegda@mail.ru>
2017-11-23 20:45:55 +0600

    made land elements orientation

---
+++ app/src/main/java/max/com/client/fragments/InputFragments.java
@@ -0,0 +1,23 @@
+package max.com.client.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import max.com.client.R;
+
+/**
+ * Created by Maxim on 11/20/2017.
+ */
+
+public class InputFragments extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.input_fragment, container, false);
+    }
+}
